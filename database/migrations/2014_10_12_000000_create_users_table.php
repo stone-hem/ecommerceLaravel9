7572d97ae76ff2f553d48f3287706f132ffa2931
Hemstone Hemingway <hemstone456@gmail.com>
2022-03-11 17:02:06 +0300

    login register, bootstrap

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->tinyInteger('role')->default('0');
+            $table->tinyInteger('active')->default('1');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
